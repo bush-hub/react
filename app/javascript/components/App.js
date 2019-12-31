f1b62ad290548961c6bbf23e46afa8f915d05edf
bush-hub <rmsdnjs1100@naver.com>
2019-12-31 14:05:12 +0000

    app

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -7,7 +7,7 @@ class App extends Component {
 			<div>
 				<h1>에베베</h1>
 				<h2>레스트</h2>
-				<h3>산이왔어요</h3>
+				<h3>산이왔어용</h3>
 				<input type='text' placeholder='이름' />
 				<input type='submit' value='제출' />
 			</div>
